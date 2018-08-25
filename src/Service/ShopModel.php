79f98307efd1b21b8554b59d5a2912791f0642f3
twinh <twinhuang@qq.com>
2018-08-25 16:02:42 +0800

    feat: 覆盖setPlainPassword方法

--- src/Service/ShopModel.php
+++ src/Service/ShopModel.php
@@ -2,15 +2,25 @@
 
 namespace Miaoxing\Shop\Service;
 
-use Miaoxing\Plugin\BaseModelV2;
+use Miaoxing\Plugin\Model\CastTrait;
+use Miaoxing\Plugin\Model\GetSetTrait;
+use Miaoxing\Plugin\Model\QuickQueryTrait;
 use Miaoxing\Shop\Metadata\ShopTrait;
 
 /**
  * ShopModel
  */
-class ShopModel extends BaseModelV2
+class ShopModel extends Shop
 {
     use ShopTrait;
+    use CastTrait;
+    use QuickQueryTrait;
+    use GetSetTrait;
 
     protected $table = 'shop';
+
+    public function afterFind()
+    {
+        //
+    }
 }
